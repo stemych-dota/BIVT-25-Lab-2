24438c8e1b20750a815f26e81029c19fda4f1767
stemych-dota <s.temych@gmail.com>
2025-10-05 21:04:10 +0300

    Update Green.cs

--- Lab2/Green.cs
+++ Lab2/Green.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace Lab2
 {
@@ -11,7 +11,10 @@ namespace Lab2
             double answer = 0;
 
             // code here
-
+            for (int i = 2; i <= n; i += 2)
+            {
+                answer += (double)i / (i + 1);
+            }
             // end
 
             return answer;
@@ -21,6 +24,14 @@ namespace Lab2
             double answer = 0;
 
             // code here
+            answer += 1;
+
+            double dl = x;
+            for (int i = 1; i <= n; i++)
+            {
+                answer += 1 / dl;
+                dl *= x;
+            }
 
             // end
 
@@ -31,7 +42,13 @@ namespace Lab2
             long answer = 0;
 
             // code here
-
+            long f = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                f *= i;
+                answer += f;
+            }
+            answer += 1;
             // end
 
             return answer;
@@ -41,7 +58,12 @@ namespace Lab2
             double answer = 0;
 
             // code here
-
+            double paw = x;
+            for (int i = 1; Math.Abs(Math.Sin(i * paw)) >= E; i++)
+            {
+                answer += Math.Sin(i * paw);
+                paw *= x;
+            }
             // end
 
             return answer;
@@ -51,7 +73,13 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            double zn = x;
+            while (Math.Abs(1 / zn - x / zn) >= E)
+            {
+                answer += 1;
+                zn *= x;
+            }
+            answer += 1;
             // end
 
             return answer;
@@ -61,7 +89,13 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            int elem = 1; int i = 0;
+            while (elem < limit)
+            {
+                elem *= 2;
+                answer += elem;
+                i++;
+            }
             // end
 
             return answer;
@@ -72,7 +106,11 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            while (L > Da)
+            {
+                L = L / 2;
+                answer++;
+            }
             // end
 
             return answer;
@@ -83,7 +121,20 @@ namespace Lab2
             double SY = 0;
 
             // code here
-
+            for (double x = a; x <= b + E; x += h)
+            {
+                SY += Math.Atan(x);
+                int sn = -1;
+                double mn = 1;
+                for (int i = 0; ; i++)
+                {
+                    double s = -1 * sn * x * mn / (2 * i + 1);
+                    SS += s;
+                    sn *= -1;
+                    mn *= x * x;
+                    if (Math.Abs(s) < E) {break; }
+                }
+            }
             // end
 
             return (SS, SY);
